1eb3539c0862f8db0a86eb26851600d84eafc576
bu23 <70064197+bu23@users.noreply.github.com>
2021-01-12 13:10:05 +0900

    view配置

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :avatar
+
+  validates_format_of :password, with: /([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])/
+  with_options presence: true do
+    validates :name
+    validates :password_confirmation
+  end
 end
