6deb7682b1a284208a12608a5f3de4f34cd644e1
bu23 <70064197+bu23@users.noreply.github.com>
2021-01-12 13:58:37 +0900

    テストコード 記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one_attached :avatar
+  has_one_attached :image
 
   validates_format_of :password, with: /([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])/
   with_options presence: true do
