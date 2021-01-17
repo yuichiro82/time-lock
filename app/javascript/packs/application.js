951ee4a4ea1327c3365c5beec326c1c2b34166d6
bu23 <70064197+bu23@users.noreply.github.com>
2021-01-17 10:17:05 +0900

    gemインストール

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,12 +4,14 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-// require("turbolinks").start()
+require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("../timer")
 require("chartkick")
 require("chart.js")
+require("../calender")
+require("../main")
 
 
 //= require audiojs
