e7cb4ecb2934260c7ab204f63e7d840bfe402e15
bu23 <70064197+bu23@users.noreply.github.com>
2021-01-23 12:27:16 +0900

    イベント表示

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,7 +5,7 @@ class EventsController < ApplicationController
     @events = Event.all
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @events }
+      format.json { render :index }
     end
   end
 
--- app/javascript/calender.js
+++ app/javascript/calender.js
@@ -6,10 +6,26 @@ import interactionPlugin from '@fullcalendar/interaction';
 
 document.addEventListener('DOMContentLoaded', function() {
   var calendarEl = document.getElementById('calendar');
-
+  eventSources: [
+    {
+      url: '/events.json',
+      method: 'POST',
+      extraParams: {
+        custom_param1: 'something',
+        custom_param2: 'somethingelse'
+      },
+      failure: function() {
+        alert('there was an error while fetching events!');
+      },
+      color: 'yellow',   // a non-ajax option
+      textColor: 'black' // a non-ajax option
+    }
+  ]
   var calendar = new Calendar(calendarEl, {
     plugins: [ dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin ],
-    initialView: 'dayGridMonth'
+    initialView: 'dayGridMonth',
+    selectable: true,
+    events: '/events.json'
   });
   calendar.render();
 });
