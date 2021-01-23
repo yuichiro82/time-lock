81967b409112f5c62c04a158a71b43011c596108
bu23 <70064197+bu23@users.noreply.github.com>
2021-01-23 14:11:55 +0900

    カレンダー修正２

--- app/javascript/calender.js
+++ app/javascript/calender.js
@@ -6,26 +6,21 @@ import interactionPlugin from '@fullcalendar/interaction';
 
 document.addEventListener('DOMContentLoaded', function() {
   var calendarEl = document.getElementById('calendar');
-  eventSources: [
-    {
-      url: '/events.json',
-      method: 'POST',
-      extraParams: {
-        custom_param1: 'something',
-        custom_param2: 'somethingelse'
-      },
-      failure: function() {
-        alert('there was an error while fetching events!');
-      },
-      color: 'yellow',   // a non-ajax option
-      textColor: 'black' // a non-ajax option
-    }
-  ]
   var calendar = new Calendar(calendarEl, {
     plugins: [ dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin ],
     initialView: 'dayGridMonth',
     selectable: true,
-    events: '/events.json'
+    events: '/events.json',
+    timeFormat: "HH:mm",
+    locale: 'ja',
+    timezone: "Tokyo",
+    dateClick: function(info) {
+      alert('Clicked on: ' + info.dateStr);
+      alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
+      alert('Current view: ' + info.view.type);
+      // change the day's background color just for fun
+      info.dayEl.style.backgroundColor = 'red';
+    }
   });
   calendar.render();
 });
