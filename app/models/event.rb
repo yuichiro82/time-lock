a3cdd91d5604a2950019acf5fc18e1fa87a84c6a
bu23 <70064197+bu23@users.noreply.github.com>
2021-01-23 14:46:26 +0900

    testコード記載

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,9 @@
 class Event < ApplicationRecord
   belongs_to :user
+
+  with_options presence: true do
+    validates :title
+    validates :start_time
+    validates :end_time
+  end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'eventデータ登録' do
+    before do
+      @event = FactoryBot.build(:event)
+    end
+
+    it '全ての値が入力されていれば保存できる事' do
+      expect(@event).to be_valid
+    end
+
+    it 'titleが空では保存ができないこと' do
+      @event.title = nil
+      @event.valid?
+      expect(@event.errors.full_messages).to include("Title can't be blank")
+    end
+
+    it 'start_timeが空で保存できない事' do
+      @event.start_time = nil
+      @event.valid?
+      expect(@event.errors.full_messages).to include("Start time can't be blank")
+    end
+
+    it 'end_timeは半角数字のみしか保存ができないこと' do
+      @event.end_time = nil
+      @event.valid?
+      expect(@event.errors.full_messages).to include("End time can't be blank")
+    end
+
+    it 'user_idが空で保存できない事' do
+      @event.user_id = nil
+      @event.valid?
+      expect(@event.errors.full_messages).to include('User must exist')
+    end
+
+    it 'userがサインインしていないと保存できない事' do
+      user_signed_in?
+      @event.valid?
+      expect(@event.errors.full_messages).to include('User must exist')
+    end
+  end
 end
