a5e50c2f0365a61ecf0950424a95f797c422325e
bu23 <70064197+yuichiro82@users.noreply.github.com>
2021-03-07 22:52:27 +0900

    tool修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :linked_dirs,
 set :rbenv_type, :user
 set :rbenv_ruby, '2.6.5'
 
-set :ssh_options, auth_methods: ['publickey'], keys: ['~/.ssh/sample.pem']
+set :ssh_options, auth_methods: ['publickey'], keys: ['~/.ssh/tool.pem']
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
